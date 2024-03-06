deafdf31dd48d36d27fea442a2bec979e4382cdb
Łukasz Olbrycht <lukasz.olbrycht@gmail.com>
2024-03-06 10:47:38 +0100

    Add checking id in edit post

--- src/components/features/EditPostForm/EditPostForm.js
+++ src/components/features/EditPostForm/EditPostForm.js
@@ -2,6 +2,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import PostForm from "../PostForm/PostForm";
 import { useDispatch, useSelector } from "react-redux";
 import { editPost, getPostById } from "../../../redux/postsRedux";
+import { Navigate } from 'react-router-dom';
 
 const EditPostForm = () => {
     const {id} = useParams();
@@ -15,7 +16,8 @@ const EditPostForm = () => {
         dispatch(editPost({id, ...post}));
         navigate('/post/' + id);
     };
-  return(
+  if (!post) return <Navigate to='/' />
+  else return(
     <PostForm action={handleSubmit} actionText={'Save changes'} title={post.title} author={post.author} shortDescription={post.shortDescription} content={post.content} publishedDate={post.publishedDate} /> 
   );
 };
